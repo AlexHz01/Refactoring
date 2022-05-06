d0197fdc90e79bd51626e6e6c2f20e6fad72d45c
AlexHz01 <yagamilonzoro@gmail.com>
2022-05-05 21:50:10 -0500

    Corrigiendo test de Explorer

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -2,21 +2,21 @@ const ExplorerService = require("./../../lib/services/ExplorerService")
 
 describe("Test para Explorer service", () =>{
     test("Requerimiento 1 y 2 para visualizar todos los explorers en el explores.json", () =>{
-        const explorers = [{mission : "node"}, {mission : "java"}, {mission : "node"}];
+        const explorers =[{ mission : "node"}, {mission : "java"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "java")
         expect(explorersInNode.length).toBe(1);
     })
 
     test("Requerimiento 2 obtener el numero de usuario que es tan en node ", () => {
         const explorers = [{ githubUsername : "ajolonauta6", mission : "node"},{ githubUsername : "ajolonauta7",mission : "java"},{ githubUsername : "ajolonauta8", mission : "node"}]
-        const nameUserInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "java")
-        expect(nameUserInNode).toBe(1)
+        const nameUserInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+        expect(nameUserInNode).toBe(2)
     })
 
     test("Requerimiento 3 obtener la lista de usuario que es tan en node ", () => {
-        const explorers = [{ githubUsername : "ajolonauta6,", mission : "node"},{ githubUsername : "ajolonauta7,",mission : "java"},{ githubUsername : "ajolonauta8,",mission : "node"}]
-        const nameUserInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "java")
-        expect(nameUserInNode).toBe(1)
+        const explorers = [{ githubUsername : "ajolonauta6", mission : "node"}, { githubUsername : "ajolonauta7", mission : "java"}];
+        const nameUserInNodes = ExplorerService.getExplorersUsernamesByMission(explorers, "node")
+        expect(nameUserInNodes).toEqual(["ajolonauta6"])
     })
 
 })
