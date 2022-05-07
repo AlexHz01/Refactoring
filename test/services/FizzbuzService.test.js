31d48a8fe3193d8f2c6e53100abf58a3c07ffad9
AlexHz01 <yagamilonzoro@gmail.com>
2022-05-06 21:45:51 -0500

    Ultima modificaion test de explorer, fizzbizz y reader

--- test/services/FizzbuzService.test.js
+++ test/services/FizzbuzService.test.js
@@ -1,18 +1,24 @@
-const FizzbuzService = require("./../../lib/services/FizzbuzzService")
+const FizzbuzService = require("./../../lib/services/FizzbuzzService");
 
 describe("Test para Fizzbuzz Services", () =>{
-    test("Verification", () =>{
+    test("1. Verification", () =>{
         const explorer1 = {name: "Explorer1", score: 1 };
         const explorer3 = {name: "Explorer3", score: 3 };
         const explorer5 = {name: "Explorer5", score: 5 };
-        const explorer15 = {name: "Explorer15", score: 30 };
-        const assignTrick1 = FizzbuzService.applyValidationInExplorer(explorer1)
-        const assignTrick3 = FizzbuzService.applyValidationInExplorer(explorer3)
-        const assignTrick5 = FizzbuzService.applyValidationInExplorer(explorer5)
-        const assignTrick15 = FizzbuzService.applyValidationInExplorer(explorer15)
+        const explorer30 = {name: "Explorer30", score: 30 };
+        const assignTrick1 = FizzbuzService.applyValidationInExplorer(explorer1);
+        const assignTrick3 = FizzbuzService.applyValidationInExplorer(explorer3);
+        const assignTrick5 = FizzbuzService.applyValidationInExplorer(explorer5);
+        const assignTrick15 = FizzbuzService.applyValidationInExplorer(explorer30);
         expect(assignTrick1).toEqual({name: "Explorer1", score: 1, trick: 1} );
         expect(assignTrick3).toEqual({name: "Explorer3", score: 3, trick: "FIZZ"} );
         expect(assignTrick5).toEqual({name: "Explorer5", score: 5, trick: "BUZZ"} );
-        expect(assignTrick15).toEqual({name: "Explorer15", score: 30, trick: "FIZZBUZZ"} );
-    })
-})
+        expect(assignTrick15).toEqual({name: "Explorer30", score: 30, trick: "FIZZBUZZ"} );
+    });
+    test("2. Verificacion sin usuario", () => {
+        const assignumber = FizzbuzService.applyValidationInNumber(5);
+        expect(assignumber).toEqual("BUZZ");
+    });
+});
+
+
